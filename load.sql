2effe1a0de20a2decdfc159b928e3b7061cc4377
Petra Janouchova <petra.janouchova@gmail.com>
2016-10-17 18:26:53 +1100

    Petra's addition of the Collective name table

--- load.sql
+++ load.sql
@@ -12,9 +12,24 @@ create table administrative_keywords (
 "Author or Creator >" text
 );
 
+create table colective_name (
+"Record ID" integer primary key,
+"Ethnic name" text,
+"Group Name Category" text,
+"Origin" text,
+"Typology of ethnic name" text,
+"Short summary" text,
+"Mappable location" point,
+"Author or Creator" text
+);
+
 
 COPY administrative_keywords 
 FROM '/home/mq20151400/Heurist_files/Administrative_keywords.txt'
+'/home/petra/Github/Heurist_files/Administrative_keywords.txt'
+'/home/petra/Github/Heurist_files/Collective_Name.txt'
+
 WITH DELIMITER ',' CSV HEADER;
 
 select * from administrative_keywords;
+select * from collective_name;
